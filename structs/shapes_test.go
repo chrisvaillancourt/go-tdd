224f26f417f2b83a23429f05b0b94fc762fea638
Chris Vaillancourt <cvaillan22@gmail.com>
2022-03-20 20:44:43 -0700

    feat(structs): add circle methods

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -2,6 +2,10 @@ package structs
 
 import "testing"
 
+type Shape interface {
+	Area() float64
+}
+
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
@@ -14,11 +18,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 5.0}
-	got := Area(rectangle)
-	want := 50.0
-	if got != want {
-		// f for float64 and .2 for 2 decimal places
-		t.Errorf("got %.2f want %.2f", got, want)
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 5.0}
+		want := 50.0
+		checkArea(t, rectangle, want)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+
 }
